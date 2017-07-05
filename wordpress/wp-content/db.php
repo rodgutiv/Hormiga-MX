7acc80eef172c16ee0c0b8163c26a7aa9b0cfaf4
rodgutiv <gutierrez.v.rod@gmail.com>
2017-07-05 14:32:33 -0500

    Abling log errors and debug options on PostgreSQL

--- wordpress/wp-content/db.php
+++ wordpress/wp-content/db.php
@@ -15,10 +15,10 @@ if( !defined('PG4WP_ROOT'))
 define('DB_DRIVER', 'pgsql'); // 'pgsql' or 'mysql' are supported for now
 
 // Set this to 'true' and check that `pg4wp` is writable if you want debug logs to be written
-define( 'PG4WP_DEBUG', false);
+define( 'PG4WP_DEBUG', true);
 // If you just want to log queries that generate errors, leave PG4WP_DEBUG to "false"
 // and set this to true
-define( 'PG4WP_LOG_ERRORS', false);
+define( 'PG4WP_LOG_ERRORS', true);
 
 // If you want to allow insecure configuration (from the author point of view) to work with PG4WP,
 // change this to true
